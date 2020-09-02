95a9cec0a8a05671a1105ba40421393707e60011
Dario Nievas <darionievas210@gmail.com>
2020-09-02 16:11:00 -0300

    Foro4 terminado para subir

---
+++ Clase1Foro4/src/clase1foro4/Controlador.java
@@ -0,0 +1,12 @@
+package clase1foro4;
+
+
+public class Controlador{
+    
+    public static void main(String[] args) {
+      Vista.leerAtributo("Dario");
+      
+      Vista.escribirAtributo("Tarea","Finalizada");
+        
+    }
+}
---
+++ Clase1Foro4/src/clase1foro4/Vista.java
@@ -0,0 +1,14 @@
+package clase1foro4;
+
+public class Vista {
+
+    public static String leerAtributo (String A){
+            System.out.println(A);
+            return A;
+        }
+    
+    public static void escribirAtributo (String a, String b){
+        System.out.println(a+" "+b);
+    }
+    
+}
