165709ad14cdca021ad78db885c49ae2b8b877d7
darionievas210 <darionievas210@gmail.com>
2020-09-11 12:24:35 -0300

    metodo burbuja

--- Clase2Foro1/src/Clase2Foro1/Foro1.java
+++ Clase2Foro1/src/Clase2Foro1/Foro1.java
@@ -1,5 +1,4 @@
 package Clase2Foro1;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Foro1 {
@@ -7,14 +6,14 @@ public class Foro1 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
-        System.out.println("Dimencionaremos un vector");
+        System.out.println("Dimencionaremos un vector ");
         System.out.println("");
         System.out.print("Escriba el tamaño del vector: ");
         int elementos = teclado.nextInt();
         System.out.println("");
         
         int [] Vector = new int[elementos];
-        
+              
         for(int i=0;i<elementos;i++){
             
             System.out.print("ingrese valor para la posicion "+(i+1)+":");
@@ -22,7 +21,7 @@ public class Foro1 {
             Vector[i]=datos;
             
         }
-        
+        //----------------------------------------------------------------------
         System.out.println("");
         System.out.println("---------------------------");
         System.out.println("NO ALMACENA MULTIPLOS DE 7");
@@ -38,24 +37,29 @@ public class Foro1 {
             }
             
         }
-        
-        Arrays.sort(Vector); //Ordena los valores del vector de menor a mayor
-        
+        //----------------------------------------------------------------------   
         System.out.println("");
         System.out.println("----------------------------");
         System.out.println("ORDENADOS DE MENOR A MAYOR");
         System.out.println("");
         
-        for(int i=0;i<elementos;i++){
-           
-            if(Vector[i]%7==0){
-                System.out.println("- (No almacena multiplos de 7)");
-            }
-            else{
-                System.out.println(Vector[i]);
-            }
-            
-        }
-        
+       //Metodo burbuja
+       
+       int aux;
+       
+       for(int i=0; i<(elementos-1);i++){
+           for(int j=0; j<(elementos-1);j++){
+               if(Vector[j] > Vector[j+1]){ //Si numero actual < numero siguiente : cambio
+                   aux = Vector[j];
+                   Vector[j] = Vector[j+1];
+                   Vector[j+1] = aux;       
+               }  
+           } 
+       }
+       //Mostrar Vector ordenado en pantalla
+       
+       for(int i=0; i<elementos; i++){
+           System.out.println(Vector[i]);
+       }    
     }
 }
